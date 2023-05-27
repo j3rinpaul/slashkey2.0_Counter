06af5a8f4e1f777498f3ae1c7bba01627baa5783
j3rin <jerrymic2255@gmail.com>
2023-05-27 09:27:10 +0530

    hours into

--- script.js
+++ script.js
@@ -67,13 +67,83 @@
 // });
 
 
-let countdownDate = new Date().setHours(new Date().getHours() + 30);
+// let countdownDate = new Date().setHours(new Date().getHours() + 30);
+// let timerInterval;
+// let isPaused = false;
+// const pauseButton = document.getElementById("pause-button");
+
+// const daysElem = document.getElementById("days"),
+//   hoursElem = document.getElementById("hours"),
+//   minutesElem = document.getElementById("minutes"),
+//   secondsElem = document.getElementById("seconds"),
+//   timer = document.getElementById("timer"),
+//   content = document.getElementById("content");
+
+// const formatTime = (time, string) => {
+//   return time == 1 ? `${time} ${string}` : `${time} ${string}`;
+// };
+
+// console.log("to edit the timer use setTime(hours) in console eg:setTime(60)")
+// const startCountdown = () => {
+//   if (isPaused) return;
+
+//   const now = new Date().getTime();
+//   const countdown = new Date(countdownDate).getTime();
+
+//   const difference = (countdown - now) / 1000;
+
+//   if (difference < 1) {
+//     endCountdown();
+//   }
+
+//   let days = Math.floor(difference / (60 * 60 * 24));
+//   let hours = Math.floor((difference % (60 * 60 * 24)) / (60 * 60));
+//   let minutes = Math.floor((difference % (60 * 60)) / 60);
+//   let seconds = Math.floor(difference % 60);
+
+//   daysElem.innerHTML = formatTime(days, "");
+//   hoursElem.innerHTML = formatTime(hours, "");
+//   minutesElem.innerHTML = formatTime(minutes, "");
+//   secondsElem.innerHTML = formatTime(seconds, "");
+// };
+
+// const endCountdown = () => {
+//   clearInterval(timerInterval);
+//   timer.remove();
+//   content.classList.add("visible");
+// };
+
+// const setCountdownTime = (hours) => {
+//   countdownDate = new Date().setHours(new Date().getHours() + hours);
+// };
+
+// const togglePause = () => {
+//   isPaused = !isPaused;
+//   pauseButton.innerHTML = isPaused ? "Resume" : "Pause";
+// };
+
+// window.addEventListener("load", () => {
+//   startCountdown();
+//   timerInterval = setInterval(startCountdown, 1000);
+// });
+
+// pauseButton.addEventListener("click", togglePause);
+
+// // Console commands
+// const setTimeCommand = (hours) => {
+//   setCountdownTime(hours);
+//   console.log(`Countdown time set to ${hours} hours.`);
+// };
+
+// // Register the console command
+// window.setTime = setTimeCommand;
+
+let countdownDate = new Date().getTime() + (30 * 60 * 60 * 1000); // Set countdown time to 30 hours from now
 let timerInterval;
 let isPaused = false;
 const pauseButton = document.getElementById("pause-button");
 
-const daysElem = document.getElementById("days"),
-  hoursElem = document.getElementById("hours"),
+const hoursElem = document.getElementById("hours"),
   minutesElem = document.getElementById("minutes"),
   secondsElem = document.getElementById("seconds"),
   timer = document.getElementById("timer"),
@@ -83,28 +153,23 @@ const formatTime = (time, string) => {
   return time == 1 ? `${time} ${string}` : `${time} ${string}`;
 };
 
-console.log("to edit the timer use setTime(hours) in console eg:setTime(60)")
 const startCountdown = () => {
   if (isPaused) return;
 
   const now = new Date().getTime();
-  const countdown = new Date(countdownDate).getTime();
-
-  const difference = (countdown - now) / 1000;
+  const difference = (countdownDate - now) / 1000;
 
   if (difference < 1) {
     endCountdown();
   }
 
-  let days = Math.floor(difference / (60 * 60 * 24));
-  let hours = Math.floor((difference % (60 * 60 * 24)) / (60 * 60));
+  let hours = Math.floor(difference / (60 * 60));
   let minutes = Math.floor((difference % (60 * 60)) / 60);
   let seconds = Math.floor(difference % 60);
 
-  daysElem.innerHTML = formatTime(days, "");
-  hoursElem.innerHTML = formatTime(hours, "");
-  minutesElem.innerHTML = formatTime(minutes, "");
-  secondsElem.innerHTML = formatTime(seconds, "");
+  hoursElem.innerHTML = formatTime(hours, " ");
+  minutesElem.innerHTML = formatTime(minutes, " ");
+  secondsElem.innerHTML = formatTime(seconds, " ");
 };
 
 const endCountdown = () => {
@@ -114,7 +179,7 @@ const endCountdown = () => {
 };
 
 const setCountdownTime = (hours) => {
-  countdownDate = new Date().setHours(new Date().getHours() + hours);
+  countdownDate = new Date().getTime() + (hours * 60 * 60 * 1000);
 };
 
 const togglePause = () => {
